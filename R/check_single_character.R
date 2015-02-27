99d5c2d5a7c2b08d3f5332e41b407d831a5c6c53
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-02-27 12:08:07 +0100

    add check_single_character()

---
+++ R/check_single_character.R
@@ -0,0 +1,22 @@
+#' Check if the object is a single character
+#' 
+#' Factors are converted to character.
+#' @param x the object to check
+#' @param name the name of the object to use in the error message
+#' @return The function gives the single character back. it throws an error when the input is not a signle character.
+#' @export
+#' @examples
+#' check_single_strictly_positive_integer(20)
+check_single_character <- function(x, name = "x"){
+  if(!class(x) %in% c("character", "factor")){
+    stop(name, " must be character")
+  }
+  if(length(x) != 1){
+    stop(name, " must be a single character")
+  }
+  if(is.factor(x)){
+    return(levels(x)[x])
+  } else {
+    return(x)
+  }
+}
