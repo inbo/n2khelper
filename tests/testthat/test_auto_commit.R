687e204f8f5476f76c54915243c1b1ba95277739
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-04 16:55:12 +0100

    unit tests for auto_commit()

---
+++ tests/testthat/test_auto_commit.R
@@ -0,0 +1,71 @@
+context("autocommit changes")
+describe("auto_commit()", {
+  package <- "test"
+  user <- "test"
+  pwd <- "test"
+  
+  library(git2r)
+  # function to create and stage a file
+  dummy_add <- function(repo.path){
+    content <- paste(sample(letters, 8, replace = TRUE), collapse = "")
+    writeLines(content, paste(repo.path, content, sep = "/"))
+    add(repository(repo.path), content)
+  }
+  
+  # create test repository
+  origin.path <- tempfile(pattern="git2r-")
+  repo.path <- tempfile(pattern="git2r-")
+  dir.create(origin.path)
+  dir.create(repo.path)
+  repo_bare <- init(origin.path, bare = TRUE)
+  repo <- clone(origin.path, repo.path)
+  dummy_add(repo.path)
+  commit(repo, "inital")
+  push(repo, "origin", "refs/heads/master")
+  
+  it("gives a warning when no username and password are provided and returns TRUE", {
+    dummy_add(repo.path = repo.path)
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path),
+      gives_warning("changes committed but not pushed")
+    )
+    dummy_add(repo.path = repo.path)
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path),
+      is_true()
+    )
+  })
+  it("returns TRUE when nothing to commit", {
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path),
+      is_true()
+    )
+  })
+  it("returns TRUE when nothing to commit", {
+    dummy_add(repo.path = repo.path)
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path, username = user),
+      throws_error("No password provided. Changes committed but not pushed.*")
+    )
+    dummy_add(repo.path = repo.path)
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path, password = pwd),
+      throws_error("No username provided. Changes committed but not pushed.*")
+    )
+  })
+  it("returns TRUE when changes are pushed", {
+    dummy_add(repo.path = repo.path)
+    expect_that(
+      auto_commit(package = package, repo.path = repo.path, username = user, password = pwd),
+      is_true()
+    )
+  })
+  it("writes a correct message title", {
+    dummy_add(repo.path = repo.path)
+    auto_commit(package = package, repo.path = repo.path, username = user, password = pwd)
+    expect_that(
+      reflog(repo)[[1]]@message,
+      is_identical_to(paste("commit: Automatic commit from", package))
+    )
+  })
+})
