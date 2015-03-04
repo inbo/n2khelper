c7d59a1591f348ae18fb820ebbb89efc0db4cde5
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-04 16:54:55 +0100

    auto_commit() pushes the commit

--- R/auto_commit.R
+++ R/auto_commit.R
@@ -1,10 +1,13 @@
 #' Commit staged changes in a git repository with automated message
+#' 
 #' The mesagge is based on the information returned by \code{\link[utils]{sessionInfo}}
 #' @param package The name of the package from which we autocommit
 #' @param repo.path The path of the repository. Default to \code{rawdata.path}
+#' @param username the username for the git repository
+#' @param password the user's password for the git repository
 #' @export
-#' @importFrom git2r commit
-auto_commit <- function(package, repo.path = rawdata.path){
+#' @importFrom git2r repository commit cred_user_pass head push
+auto_commit <- function(package, repo.path = rawdata.path, username, password){
   package <- check_single_character(package)
   
   # define the repository
@@ -40,4 +43,21 @@ auto_commit <- function(package, repo.path = rawdata.path){
   if("error" %in% class(committed)){
     stop(committed)
   }
+  if(class(committed) != "git_commit"){
+    return(invisible(TRUE))
+  }
+  if(missing(username) & missing(password)){
+    warning("changes committed but not pushed")
+    return(invisible(TRUE))
+  }
+  if(missing(username)){
+    stop("No username provided. Changes committed but not pushed.")
+  }
+  if(missing(password)){
+    stop("No password provided. Changes committed but not pushed.")
+  }
+  credentials <- cred_user_pass(username = username, password = password)
+  push(head(repo), credentials = credentials)
+  
+  return(invisible(TRUE))
 }
