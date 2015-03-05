b9c3a7891e8351df0ecf496700f09f32a81ad09c
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-05 14:37:21 +0100

    add unit test for read_delim_test()

--- R/read_delim_git.R
+++ R/read_delim_git.R
@@ -7,9 +7,11 @@ read_delim_git <- function(file, path, repo.path = rawdata.path){
   file <- check_single_character(x = file, name = "file")
   path <- check_single_character(x = path, name = "path")
   repo.path <- check_single_character(x = repo.path, name = "repo.path")
+  repo.path <- normalizePath(repo.path, winslash = "/", mustWork = FALSE)
   if(!is_git_repo(path = repo.path)){
-    stop(repo.path, "is not a git repository")
+    stop(repo.path, " is not a git repository")
   }
+  
   filename <- paste(repo.path, path, file, sep = "/")
   read.delim(filename)
 }
---
+++ tests/testthat/test_read_delim_git.R
@@ -0,0 +1,26 @@
+context("read data.frame from git")
+describe("read_delim_git()", {
+  library(git2r)
+  file <- "test.txt"
+  path <- "test"
+  repo.path <- normalizePath(tempfile(pattern="git2r-"), winslash = "/", mustWork = FALSE)
+  df <- data.frame(x = 1, y = 1:10)
+  
+  
+  it("stops is repo.path is not a git repository", {
+    expect_that(
+      read_delim_git(file = file, path = path, repo.path = repo.path),
+      throws_error(paste(repo.path, "is not a git repository.*"))
+    )
+  })
+
+  dir.create(repo.path)
+  repo <- init(repo.path)
+  write_delim_git(x = df, file = file, path = path, repo.path = repo.path)
+  it("read the tab-delimited file", {
+    expect_that(
+      read_delim_git(file = file, path = path, repo.path = repo.path),
+      is_equivalent_to(df)
+    )
+  })
+})
