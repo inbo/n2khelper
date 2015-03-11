02f54ce7939e18607259419566734e07dd709f8c
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-11 17:48:23 +0100

    add list_files_git()

---
+++ R/list_files_git.R
@@ -0,0 +1,20 @@
+#' List the files in a path of a git repository
+#'@inheritParams read_delim_git
+#'@inheritParams base::list.files
+#'@export
+list_files_git <- function(path, pattern = "^[0123456789].*\\.txt$", repo.path = rawdata.path){
+  path <- check_single_character(x = path, name = "path")
+  repo.path <- check_single_character(x = repo.path, name = "repo.path")
+  repo.path <- normalizePath(repo.path, winslash = "/", mustWork = FALSE)
+  if(!is_git_repo(path = repo.path)){
+    stop(repo.path, " is not a git repository")
+  }
+
+  full.path <- paste(repo.path, path, sep = "/")
+  full.path <- normalizePath(full.path, winslash = "/", mustWork = FALSE)
+  if(file_test("-d", full.path)){
+    list.files(path = full.path, pattern = pattern)
+  } else {
+    stop(full.path, " is not a directory")
+  }
+}
---
+++ tests/testthat/test_list_files_git.R
@@ -0,0 +1,49 @@
+context("list files for a git repository")
+describe("list_files_git()", {
+  library(git2r)
+  files <- sort(c("test.txt", "0123456.txt", "test", "0123456"))
+  path <- "test"
+  repo.path <- tempfile(pattern="git2r-")
+  repo.path <- normalizePath(repo.path, winslash = "/", mustWork = FALSE)
+  
+  
+  it("stops is repo.path is not a git repository", {
+    expect_that(
+      list_files_git(path = path, repo.path = repo.path),
+      throws_error(paste(repo.path, "is not a git repository.*"))
+    )
+  })
+
+  dir.create(repo.path)
+  repo <- init(repo.path)
+  repo.path <- normalizePath(repo.path, winslash = "/", mustWork = FALSE)
+  full.path <- paste(repo.path, path, sep = "/")
+  full.path <- normalizePath(full.path, winslash = "/", mustWork = FALSE)
+  it("stops is the path doesn't exist", {
+    expect_that(
+      list_files_git(path = path, repo.path = repo.path),
+      throws_error(paste(full.path, "is not a directory.*"))
+    )
+  })
+  
+  dir.create(paste(repo.path, path, sep = "/"))
+  file.create(paste(full.path, files, sep = "/"))
+  it("list the files according to the pattern", {
+    expect_that(
+      list_files_git(path = path, repo.path = repo.path),
+      is_identical_to(files[grep("^[0123456789].*\\.txt$", files)])
+    )
+    expect_that(
+      list_files_git(path = path, pattern = "\\.txt$", repo.path = repo.path),
+      is_identical_to(files[grep("\\.txt$", files)])
+    )
+    expect_that(
+      list_files_git(path = path, pattern = ".*", repo.path = repo.path),
+      is_identical_to(files)
+    )
+    expect_that(
+      list_files_git(path = path, pattern = ".exe", repo.path = repo.path),
+      is_identical_to(character(0))
+    )
+  })
+})
