b073ddeab09942900671faa852ee35df42cf99f8
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-25 13:24:40 +0200

    get_nbn_name() handles more ties

--- R/get_nbn_name.R
+++ R/get_nbn_name.R
@@ -8,6 +8,7 @@ get_nbn_name <- function(nbn.key){
   channel <- odbcDriverConnect(connection = nbn.dsn)
   sql <- paste0("
     SELECT
+      CASE WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%' THEN 1 ELSE 0 END AS Preference,
       ns.RECOMMENDED_TAXON_VERSION_KEY AS NBNID,
       t.LANGUAGE AS Language,
       t.ITEM_NAME AS Name, 
@@ -15,11 +16,17 @@ get_nbn_name <- function(nbn.key){
       ns.TAXON_VERSION_STATUS AS Status
     FROM
         (
-            NAMESERVER AS ns 
-          INNER JOIN 
-            TAXON_VERSION AS tv 
-          ON 
-            ns.INPUT_TAXON_VERSION_KEY = tv.TAXON_VERSION_KEY
+          (
+              NAMESERVER AS ns 
+            INNER JOIN 
+              TAXON_VERSION AS tv 
+            ON 
+              ns.INPUT_TAXON_VERSION_KEY = tv.TAXON_VERSION_KEY
+          )
+        INNER JOIN
+          TAXON_LIST_ITEM AS tli
+        ON
+          tv.TAXON_VERSION_KEY = tli.TAXON_VERSION_KEY
         )
       INNER JOIN
         TAXON AS t 
@@ -28,6 +35,7 @@ get_nbn_name <- function(nbn.key){
     WHERE
       ns.RECOMMENDED_TAXON_VERSION_KEY IN (", paste0("'", unique(nbn.key), "'", collapse = ", "), ")
     GROUP BY
+      CASE WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%' THEN 1 ELSE 0 END,
       ns.RECOMMENDED_TAXON_VERSION_KEY,
       t.LANGUAGE,
       t.ITEM_NAME, 
@@ -47,10 +55,31 @@ get_nbn_name <- function(nbn.key){
       if(nrow(this.key) == 1){
         return(this.key[, "Name", drop = FALSE])
       }
+      preference <- which(this.key$Preference == 1) 
+      if(length(preference) == 1){
+        return(this.key[preference, "Name", drop = FALSE])
+      }
+      if(length(preference) > 1){
+        this.key <- this.key[preference, ]
+      }
       status.R <- which(this.key$Status == "R")
       if(length(status.R) == 1){
         return(this.key[status.R, "Name", drop = FALSE])
       }
+      if(length(status.R) > 1){
+        print(this.key)
+        stop("yet to be written")
+      }
+      if(length(unique(this.key$Type)) == 1){
+        combined <- paste0(
+          "(", 
+          paste(this.key$Name, collapse = "/"),
+          ")"
+        )
+        warning("Multiple matching values: ", paste(this.key$Name, collapse = "/"))
+        return(data.frame(Name = combined))
+      }
+      print(this.key)
       stop("yet to be written")
     }
   )
