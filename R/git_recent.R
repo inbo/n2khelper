16bff45140dc60d491e5452703930503196a52d4
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-29 17:11:58 +0200

    add git_recent()

---
+++ R/git_recent.R
@@ -0,0 +1,59 @@
+#' Get the info from the latest commit of a file
+#' @inheritParams write_delim_git
+#' @name git_recent
+#' @rdname git_recent
+#' @exportMethod git_recent
+#' @docType methods
+#' @importFrom methods setGeneric
+#' @include git_connection.R
+setGeneric(
+  name = "git_recent", 
+  def = function(file, connection, path){
+    standard.generic(git_recent)
+  }
+)
+
+#' @rdname git_recent
+#' @aliases git_recent,git_connection-methods
+#' @importFrom methods setMethod
+setMethod(
+  f = "git_recent", 
+  signature = "ANY", 
+  definition = function(file, connection, path){
+    this.connection <- git_connection(repo.path = connection, local.path = path)
+    git_recent(file = file, connection = this.connection)
+  } 
+)
+
+#' @rdname git_recent
+#' @aliases git_recent,git_connection-methods
+#' @importFrom methods setMethod
+setMethod(
+  f = "git_recent",
+  signature = signature(connection = "git_connection"),
+  definition = function(file, connection, path){
+    file <- check_single_character(x = file, name = "file")
+    
+    old.wd <- getwd()
+    setwd(connection@Repository@path)
+    commit.info <- system(
+      paste0(
+        "git log -n 1 --date=iso ",
+        connection@LocalPath, "/", file
+      ),
+      intern = TRUE
+    )
+    setwd(old.wd)
+    date <- commit.info[grep("^Date:", commit.info)]
+    date <- as.POSIXct(date, format = "Date: %F %T %z")
+    commit <- commit.info[grep("^commit", commit.info)]
+    commit <- gsub("^commit ", "", commit)
+    author <- commit.info[grep("^Author:", commit.info)]
+    author <- gsub("^Author: ", "", author)
+    return(list(
+      Commit = commit,
+      Author = author,
+      Date = date
+    ))
+  }
+)
