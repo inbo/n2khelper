0ebdf48a3cd47b081512882b211d5fbb46e066fc
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-27 18:43:20 +0200

    check_dataframe_variable() can check the class of the variables as well

--- R/check_dataframe_variable.R
+++ R/check_dataframe_variable.R
@@ -1,7 +1,7 @@
 #' Check if a data.frame contains variables
 #' 
 #' @param df the \code{data.frame} to check
-#' @param variable a vector with the names of the variable to check
+#' @param variable either a character vector with the names of the variable to check or a named list. The names of the list must match the names of the required variables in the data.frame. The elements of the list contain the accepted classes for each varaible.
 #' @param name the name of the \code{data.frame} to use in the error message
 #' @param error When TRUE (default), the function returns an error when a variable is missing. Otherwise it returns a warning.
 #' @return The function returns TRUE when all variables are present. If returns FALSE when a variable is missing and \code{error = FALSE}.
@@ -11,12 +11,24 @@
 #'  df = data.frame(a = integer(0)),
 #'  variable = "a"
 #' )
+#' check_dataframe_variable(
+#'  df = data.frame(a = integer(0)),
+#'  variable = list(a = c("integer", "numeric"))
+#' )
 check_dataframe_variable <- function(df, variable, name = "df", error = TRUE){
+  name <- check_single_character(x = name, name = "name")
   if(!class(df) %in% c("data.frame", "matrix")){
     stop(name, " must be a data.frame")
   }
-  name <- check_single_character(x = name, name = "name")
   error <- check_single_logical(x = error, name = "error")
+  
+  if(class(variable) == "list"){
+    required.class <- variable
+    variable <- names(required.class)
+  } else {
+    required.class <- NULL
+  }
+  
   variable <- check_character(x = variable, name = "variable", na.action = na.fail)
   if(length(variable) == 0){
     stop("'variable' must contain at least one value")
@@ -31,6 +43,28 @@ check_dataframe_variable <- function(df, variable, name = "df", error = TRUE){
       warning("Variables missing in ", name, ": ", missing.var)
       return(FALSE)
     }
+  }
+  
+  if(is.null(required.class)){
+    return(TRUE)
+  }
+  
+  current.class <- sapply(df[, variable], class)
+  correct.class <- sapply(seq_along(current.class), function(i){
+    any(current.class[[i]] %in% required.class[[i]])
+  })
+  if(!all(correct.class)){
+    wrong.class <- current.class[!correct.class]
+    wrong.class <- sapply(wrong.class, paste, collapse = "', '")
+    expected.class <- required.class[names(wrong.class)]
+    expected.class <- sapply(expected.class, paste, collapse = "', '")
+    stop(
+      "Wrong class for following variable(s)\n", 
+      paste0(
+        names(wrong.class), ": got '", wrong.class, "', expected '", expected.class, "'\n", 
+        collapse = ", "
+      )
+    )
   }
   return(TRUE)
 }
