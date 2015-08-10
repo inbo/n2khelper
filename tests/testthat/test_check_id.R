1f0e794b1a306fd50c471c38099bb6170b2a4527
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-10 20:44:35 +0200

    some more skip_on_cran()

--- tests/testthat/test_check_id.R
+++ tests/testthat/test_check_id.R
@@ -6,7 +6,11 @@ describe("check_id()", {
   variable.text <- "Description"
   value.text <- "'git, tab delimited ssh'"
   sql <- paste("SELECT", variable, "FROM", table, "WHERE", variable.text, "=", value.text)
-  value <- RODBC::sqlQuery(channel = channel, query = sql)[, 1]
+  if (class(channel) == "RODBC") {
+    value <- RODBC::sqlQuery(channel = channel, query = sql)[, 1]
+  } else {
+    value <- 1
+  }
   junk <- "junk"
 
   it("tests if the channel in an ODBC connection", {
@@ -16,28 +20,34 @@ describe("check_id()", {
     )
   })
   it("tests if the table exists in the ODBC connection", {
+    skip_on_cran()
     expect_that(
       check_id(value = value, variable = variable, table = junk, channel = channel),
       throws_error(paste("Table\\(s\\) missing:", junk))
     )
   })
   it("tests if data type is correct", {
+    skip_on_cran()
     expect_that(
       check_id(value = 999999999, variable = variable.text, table = table, channel = channel),
       throws_error(".*Conversion failed when converting.*")
     )
   })
   it("tests if the variable table exists in the table", {
+    skip_on_cran()
     expect_that(
       check_id(value = value, variable = junk, table = table, channel = channel),
       throws_error(paste0("Variable\\(s\\) missing from '", table, "': ", junk))
     )
   })
   it("tests if the id exists in the table", {
+    skip_on_cran()
     expect_that(
       check_id(value = 999999999, variable = variable, table = table, channel = channel),
       is_false()
     )
   })
-  RODBC::odbcClose(channel)
+  if (class(channel) == "RODBC") {
+    RODBC::odbcClose(channel)
+  }
 })
