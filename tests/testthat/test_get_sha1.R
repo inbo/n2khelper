f116dd061cdcbebad17a9d9bdce914f7ba8ad154
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-12 13:55:55 +0200

    add more test on get_sha1 between 32-bit and 64-bit

--- tests/testthat/test_get_sha1.R
+++ tests/testthat/test_get_sha1.R
@@ -1,8 +1,6 @@
 context("calculate sha1 fingerprints")
 describe("get_sha1", {
   x.numeric <- 1.2345678901234567890123456789
-  #digest::digest(signif(x.numeric, n2khelper:::sha1_digits()), algo = "sha1")
-  x.numeric.sha1 <- "007da406bbe456a2a3b6ab258efe17cf88206600" #32-bit, 14 digit, windows
   x.list <- list(letters, x.numeric)
   x.dataframe <- data.frame(
     X = letters,
@@ -15,6 +13,24 @@ describe("get_sha1", {
   x.dataframe.round <- x.dataframe
   x.dataframe.round$Y <- signif(x.dataframe.round$Y, n2khelper:::sha1_digits())
   x.factor <- factor(letters)
+
+  cat(
+    "x.numeric.sha1 <- \"", get_sha1(x.numeric), "\"\n",
+    "x.list.sha1 <- \"", get_sha1(x.list), "\"\n",
+    "x.dataframe.sha1 <- \"", get_sha1(x.dataframe), "\"\n",
+    "x.matrix.num.sha1 <- \"", get_sha1(x.matrix.num), "\"\n",
+    "x.matrix.letter.sha1 <- \"", get_sha1(x.matrix.letter), "\"\n",
+    "x.factor.sha1 <- \"", get_sha1(x.factor), "\"\n",
+    sep = ""
+  )
+  #32-bit, 14 digit, windows
+  x.numeric.sha1 <- "007da406bbe456a2a3b6ab258efe17cf88206600"
+  x.list.sha1 <- "f07bdb7e125c84f1b319506f0a65db047429a449"
+  x.dataframe.sha1 <- "9c704b9c4b6cf7f9a09ed1c1053e0794cff16715"
+  x.matrix.num.sha1 <- "a4234514920fb1d0aabc9bda021c03497d8f4b4a"
+  x.matrix.letter.sha1 <- "c5100b97b0a506851834947557847ed1ddd2462e"
+  x.factor.sha1 <- "02031b6ef7804a28e10358985a91acca8315e989"
+
   it("tests using detailed numbers", {
     expect_that(
       identical(x.numeric, signif(x.numeric, n2khelper:::sha1_digits())),
@@ -32,10 +48,6 @@ describe("get_sha1", {
         signif(x.numeric, n2khelper:::sha1_digits()),
         algo = "sha1"
       )
-    )
-    expect_identical(
-      get_sha1(x.numeric),
-      x.numeric.sha1
     )
     expect_that(
       get_sha1(letters),
@@ -69,5 +81,13 @@ describe("get_sha1", {
       get_sha1(x.factor),
       digest::digest(x.factor, algo = "sha1")
     )
+  })
+  it("return the same SHA1 on both 32-bit and 64-bit OS", {
+    expect_identical(get_sha1(x.numeric), x.numeric.sha1)
+    expect_identical(get_sha1(x.list), x.list.sha1)
+    expect_identical(get_sha1(x.dataframe), x.dataframe.sha1)
+    expect_identical(get_sha1(x.matrix.num), x.matrix.num.sha1)
+    expect_identical(get_sha1(x.matrix.letter), x.matrix.letter.sha1)
+    expect_identical(get_sha1(x.factor), x.factor.sha1)
   })
 })
