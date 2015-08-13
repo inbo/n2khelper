eae5969fe9de01a58dbfb10c545b060b5163a8cb
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-13 12:38:00 +0200

    add tests for matrix, data.frame and list

--- tests/testthat/test_get_sha1.R
+++ tests/testthat/test_get_sha1.R
@@ -74,7 +74,19 @@ describe("get_sha1", {
     # scalar
     TRUE, FALSE, 1L, 1, "a",
     # vector
-    c(TRUE, FALSE), 1:10, seq(1, 10, length = 7), letters, factor(letters)
+    c(TRUE, FALSE), 1:3, seq(0, 10, length = 4), letters[1:3], factor(letters[4:6])
+  )
+  select.vector <- which(sapply(test.element, length) > 1)
+  test.element <- c(
+    test.element,
+    # add a data.frame
+    expand.grid(test.element[select.vector]),
+    # add a list
+    test.element[select.vector],
+    # add matrices
+    matrix(1:10),
+    matrix(seq(0, 10, length = 4)),
+    matrix(letters)
   )
   cat("\ncorrect <- c(\n")
   cat(
@@ -94,15 +106,65 @@ describe("get_sha1", {
     "6c30934a0ea2c0473d37b6d8bb5b955b435a8bc1",
     "1f9928593251410322823fefea8c3ef79b4d0254",
     "692ff1b9390cfc01625d8dbb850d04426e193889",
-    "8ecd6c9b9ee20692440f0283ad257165ec7ae75d",
-    "87ff39278d0ccb8990a96f66735ed3817511a340",
-    "174b54126178f63dcfd088a201f0c014dbb5d3b0",
-    "02031b6ef7804a28e10358985a91acca8315e989"
+    "6fdccc872a60a9170f5cb5eee74312f4cbc384af",
+    "8e67ddb0502dfe87c6d4341ab7592d98a154c676",
+    "3bc1c85261b958307340b7a8a9fcff3e2586516b",
+    "3c23872b9b4e17b16c1d640fe3c29f251202b012",
+    "8411ad341f0f5f0956b74210d4df33456139b974",
+    "70288494246a97032176bdb731cfdbc638d27efd",
+    "236a88147b4a15c6560fc9635b9233a43754ac23",
+    "4893b157a9a7553b7952f265c565acc735f449b4",
+    "7d64f263aa3566e966ce23d003296b5e9c5ae680",
+    "692ff1b9390cfc01625d8dbb850d04426e193889",
+    "6fdccc872a60a9170f5cb5eee74312f4cbc384af",
+    "8e67ddb0502dfe87c6d4341ab7592d98a154c676",
+    "3bc1c85261b958307340b7a8a9fcff3e2586516b",
+    "3c23872b9b4e17b16c1d640fe3c29f251202b012",
+    "6c30934a0ea2c0473d37b6d8bb5b955b435a8bc1",
+    "315a5aa84aa6cfa4f3fb4b652a596770be0365e8",
+    "a05091ea911bb9665d685c99b42f20e08c8a1927",
+    "f4477038cc95efbea855596fcc42fa28bc7dc9da",
+    "fbda0789bf4ef2af3fac585ccfbcb169636e8eae",
+    "e9a0e7aa182b91a3dc92d527cc4c9b1bbe5cb61a",
+    "56b37df6bb681326fd528d8e1940a77cb6dd6928",
+    "54e6f7d9f57078b6c6e4474a398a054dbf53a0e9",
+    "507ef986ec269e4854ee486ba72e00d8becc5aa3",
+    "50e27f6ab08034b9562926f6f13f8dbd233cfdc3",
+    "25dd8f33676144d5e42427eff1c8546724d63d15",
+    "c12b5c6e847d20a91a26f6f92e980094521c5d24",
+    "92eae8531a5572ab2d155ca4d9c3b6d94a1e6615",
+    "50e27f6ab08034b9562926f6f13f8dbd233cfdc3",
+    "1f9928593251410322823fefea8c3ef79b4d0254",
+    "ee6e7fdb03a0d35b3a6f499d0f8f610686551d51",
+    "8e7f9fe32c49050c5ca146150fc58b93fbeea245",
+    "e59165f73b7dc7e0d6ae94ec9aac9e8e95fd8a2c",
+    "7f608bde8f0e308aa8866d737ddebbfae9674163",
+    "86e99e22d003547538a5f446165488f7861fa2c3",
+    "ce27dce0e84ad90d3e90e9b571a73720d0fb4890",
+    "221799200137b7d72dfc4a618465bec71333a58b",
+    "13b5c7533cccc95d2f7cd18df78ea78ed9111c02",
+    "88b7c7c5f6921ec9e914488067552829a17a42a4",
+    "6127e4cdbf02f18898554c037f0d4acb95c608ab",
+    "984ca0fd9ed47ac08a31aeb88f9c9a5f905aeaa2",
+    "954da0ea9a5d0aa42516beebc5542c638161934c",
+    "7d1e34387808d9f726efbb1c8eb0819a115afb52",
+    "2e21764867596d832896d9d28d6e6489a0b27249",
+    "666881f1f74c498e0292ccf3d9d26089ee79dae7",
+    "966dbbe6cf1c43ac784a8257b57896db9fd3f357",
+    "4ab40e0c23010553e9e4c058ef58f50088f9e87c",
+    "bfa0e51b33ebd3b9a823368b7e4c357b2b98790b",
+    "fc1ba0a4718421f0050cc5e159703838f733aa59",
+    "25cce9eca8abedab78a765b49e74fba77f4463d4",
+    "9d453f3128cb2fd55684b979a11d47c97f12dc87",
+    "d612108f47c8accbeffd2d9d54c1fa7f74fb432d",
+    "ef60fa66262167e7a31398b16fa762151c6d1b28",
+    "a235e3cc7109def777a99e660b9829cea48ce9a4",
+    "d19d82f849bad81a39da932d3087a60c78de82c1"
   )
   it("return the same SHA1 on both 32-bit and 64-bit OS", {
     expect_identical(
       correct,
-      sapply(test.element, get_sha1)
+      unname(sapply(test.element, get_sha1))
     )
   })
 })
