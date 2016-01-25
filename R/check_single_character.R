deac4b00a9f8a595bbcbb2cc6c79ec045dfadfed
ThierryO <Thierry.Onkelinx@inbo.be>
2016-01-25 13:26:37 +0100

    update coding style

--- R/check_single_character.R
+++ R/check_single_character.R
@@ -1,5 +1,5 @@
 #' Check if the object is a single character
-#' 
+#'
 #' Factors are converted to character.
 #' @param x the object to check
 #' @param name the name of the object to use in the error message
@@ -9,7 +9,7 @@
 #' check_single_character("20")
 check_single_character <- function(x, name = "x"){
   x <- check_character(x = x, name = name, na.action = na.fail)
-  if(length(x) != 1){
+  if (length(x) != 1) {
     stop(name, " must be a single character")
   }
   return(x)
--- R/check_single_logical.R
+++ R/check_single_logical.R
@@ -1,5 +1,5 @@
 #' Check if the object is a single logical
-#' 
+#'
 #' @param x the object to check
 #' @param name the name of the object to use in the error message
 #' @return The function gives the single logical back. it throws an error when the input is not a single logical.
@@ -8,13 +8,13 @@
 #' check_single_logical(TRUE)
 check_single_logical <- function(x, name = "x"){
   name <- check_single_character(x = name, name = "name")
-  if(class(x) != "logical"){
+  if (class(x) != "logical") {
     stop(name, " must be logical")
   }
-  if(length(x) != 1){
+  if (length(x) != 1) {
     stop(name, " must be a single logical")
   }
-  if(is.na(x)){
+  if (is.na(x)) {
     stop(name, " must be TRUE or FALSE")
   }
   return(x)
