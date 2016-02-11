b0811ae947f667de12ae49f303bf2368264ca2c6
ThierryO <Thierry.Onkelinx@inbo.be>
2016-02-11 14:53:02 +0100

    connect_result() gains a username and password argument. It chooses a different driver under Windows than under unix

--- R/connect_result.R
+++ R/connect_result.R
@@ -2,14 +2,34 @@
 #' @export
 #' @importFrom RODBC odbcDriverConnect
 #' @param develop Logical value. Indicates the location ton the results database
-connect_result <- function(develop = TRUE){
-  develop <- check_single_logical(develop)
+#' @param username the username to connect to the database. Use a trusted connection when missing
+#' @param password the password for the username. Ignored when using a trusted connection.
+#' @importFrom assertthat assert_that is.flag noNA is.string
+connect_result <- function(develop = TRUE, username, password){
+  assert_that(is.flag(develop))
+  assert_that(noNA(develop))
   if (develop) {
-    odbcDriverConnect(
-      "Driver=SQL Server;Server=INBODEV02\\development;
-      Database=D0116on00_SoortenMeetnetAnalyse;Trusted_Connection=Yes;"
-    )
+    server <- "INBODEV02\\development"
   } else {
     stop("Production databank not yet defined")
   }
+  driver <- ifelse(.Platform$OS.type == "windows", "SQL Server", "FreeTDS")
+  if (missing(username)) {
+    authentication <- "Trusted_Connection=Yes;"
+  } else {
+    assert_that(is.string(username))
+    if (username == "") {
+      authentication <- "Trusted_Connection=Yes;"
+    } else {
+      assert_that(is.string(password))
+      authentication <- sprintf("Uid=%s;Pwd=%s", username, password)
+    }
+  }
+  connection <- sprintf(
+    "Driver=%s;Server=%s;Database=D0116on00_SoortenMeetnetAnalyse;%s",
+    driver,
+    server,
+    authentication
+  )
+  odbcDriverConnect(connection)
 }
