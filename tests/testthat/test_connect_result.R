c23d7fdbc527fa2777d14a5adf40c9f8e3edfbda
ThierryO <Thierry.Onkelinx@inbo.be>
2016-02-17 09:51:56 +0100

    bugfix: wrong brackets

--- tests/testthat/test_connect_result.R
+++ tests/testthat/test_connect_result.R
@@ -1,12 +1,12 @@
 context("connect result")
 describe("check_id()", {
-  it ("throws an error on the production version") {
+  it ("throws an error on the production version", {
     expect_error(
       connect_result(develop = FALSE),
       "Production database not yet defined"
     )
-  }
-  it ("uses trusted authentication when username is missing or ''") {
+  })
+  it ("uses trusted authentication when username is missing or ''", {
     skip_on_cran()
     skip_on_os("unix")
     expect_is(
@@ -17,5 +17,5 @@ describe("check_id()", {
       connect_result(username = ""),
       "RODBC"
     )
-  }
+  })
 })
