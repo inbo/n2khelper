416b072a57f8865dc0dbb3ef3a8bf00168dd649d
ThierryO <Thierry.Onkelinx@inbo.be>
2016-02-18 11:04:14 +0100

    add unit test for connect_result() with username and password

--- tests/testthat/test_connect_result.R
+++ tests/testthat/test_connect_result.R
@@ -18,4 +18,14 @@ describe("check_id()", {
       "RODBC"
     )
   })
+  it ("uses username and password when supplied", {
+    skip_on_cran()
+    expect_is(
+      connect_result(
+        username = Sys.getenv("N2KRESULT_USERNAME"),
+        password = Sys.getenv("N2KRESULT_PASSWORD")
+      ),
+      "RODBC"
+    )
+  })
 })
