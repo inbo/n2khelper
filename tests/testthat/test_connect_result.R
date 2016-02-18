87594a91ee96d870f9bbcb928e1e55bfe6d009e0
ThierryO <Thierry.Onkelinx@inbo.be>
2016-02-18 11:06:57 +0100

    update coding style

--- tests/testthat/test_connect_result.R
+++ tests/testthat/test_connect_result.R
@@ -1,12 +1,12 @@
 context("connect result")
 describe("check_id()", {
-  it ("throws an error on the production version", {
+  it("throws an error on the production version", {
     expect_error(
       connect_result(develop = FALSE),
       "Production database not yet defined"
     )
   })
-  it ("uses trusted authentication when username is missing or ''", {
+  it("uses trusted authentication when username is missing or ''", {
     skip_on_cran()
     skip_on_os("linux")
     expect_is(
@@ -18,7 +18,7 @@ describe("check_id()", {
       "RODBC"
     )
   })
-  it ("uses username and password when supplied", {
+  it("uses username and password when supplied", {
     skip_on_cran()
     expect_is(
       connect_result(
