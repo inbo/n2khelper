be1018bc6e64878f16f9924d0de701365872937c
ThierryO <Thierry.Onkelinx@inbo.be>
2016-07-04 11:57:11 +0200

    connect_result() uses a PostgreSQL database

--- R/connect_result.R
+++ R/connect_result.R
@@ -1,35 +1,29 @@
 #' Opens an ODBC connection to the 'results' database
 #' @export
-#' @importFrom RODBC odbcDriverConnect
-#' @param develop Logical value. Indicates the location ton the results database
-#' @param username the username to connect to the database. Use a trusted connection when missing
-#' @param password the password for the username. Ignored when using a trusted connection.
+#' @importFrom RPostgreSQL PostgreSQL
+#' @importFrom DBI dbConnect
+#' @param username the username to connect to the database.
+#' @param password the password for the username.
+#' @param develop Logical value. Indicates the location of the results database
 #' @importFrom assertthat assert_that is.flag noNA is.string
-connect_result <- function(develop = TRUE, username, password){
+connect_result <- function(username, password, develop = TRUE){
   assert_that(is.flag(develop))
   assert_that(noNA(develop))
+  assert_that(is.string(username))
+  assert_that(is.string(password))
+
+  dbname <- "n2kresult"
+  drv <- RPostgreSQL::PostgreSQL()
   if (develop) {
-    server <- "INBODEV02\\development"
+    host <- "localhost"
   } else {
     stop("Production database not yet defined")
   }
-  driver <- ifelse(.Platform$OS.type == "windows", "SQL Server", "FreeTDS")
-  if (missing(username)) {
-    authentication <- "Trusted_Connection=Yes;"
-  } else {
-    assert_that(is.string(username))
-    if (username == "") {
-      authentication <- "Trusted_Connection=Yes;"
-    } else {
-      assert_that(is.string(password))
-      authentication <- sprintf("Uid=%s;Pwd=%s", username, password)
-    }
-  }
-  connection <- sprintf(
-    "Driver=%s;Server=%s;Database=D0116on00_SoortenMeetnetAnalyse;%s",
-    driver,
-    server,
-    authentication
+  dbConnect(
+    drv = drv,
+    host = host,
+    dbname = dbname,
+    user = username,
+    password = password
   )
-  odbcDriverConnect(connection)
 }
