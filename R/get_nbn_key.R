4ec915123eac217a247e17a663552ce43e6d63a1
ThierryO <thierry.onkelinx@inbo.be>
2018-09-04 14:34:38 +0200

    update documentation on get_nbn_key()

--- R/get_nbn_key.R
+++ R/get_nbn_key.R
@@ -49,64 +49,124 @@ get_nbn_key <- function(name, language = "la", channel, authority = FALSE){
       paste0("'", available, "'", collapse = ", ")
     )
   }
-  if (authority == FALSE) {
-  output <- sprintf("
-    SELECT
-      t.ITEM_NAME AS InputName,
-      ns.RECOMMENDED_TAXON_VERSION_KEY as NBNKey,
-      tr.ITEM_NAME AS GenericName,
-      CASE
-        WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%%'
-        THEN 1 ELSE 0 END AS PreferenceInput,
-      CASE
-        WHEN tlir.TAXON_LIST_VERSION_KEY like 'INB%%'
-        THEN 1 ELSE 0 END AS PreferenceOutput,
-      tv.COMMENT AS Comment,
-      tv.Attribute
-    FROM
-      (
+  if (isTRUE(authority)) {
+    sql <- sprintf("
+      SELECT
+        t.ITEM_NAME + ' ' + t.AUTHORITY AS InputName,
+        ns.RECOMMENDED_TAXON_VERSION_KEY as NBNKey,
+        tr.ITEM_NAME AS GenericName,
+        CASE
+          WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%%'
+          THEN 1 ELSE 0 END AS PreferenceInput,
+        CASE
+          WHEN tlir.TAXON_LIST_VERSION_KEY like 'INB%%'
+          THEN 1 ELSE 0 END AS PreferenceOutput,
+        tv.COMMENT AS Comment,
+        tv.Attribute
+      FROM
         (
           (
             (
-              dbo.TAXON AS t
+              (
+                dbo.TAXON AS t
+              INNER JOIN
+                dbo.TAXON_VERSION AS tv
+              ON
+                t.TAXON_KEY = tv.TAXON_KEY
+              )
             INNER JOIN
-              dbo.TAXON_VERSION AS tv
+              dbo.TAXON_LIST_ITEM AS tli
             ON
-              t.TAXON_KEY = tv.TAXON_KEY
+              tv.TAXON_VERSION_KEY = tli.TAXON_VERSION_KEY
             )
           INNER JOIN
-            dbo.TAXON_LIST_ITEM AS tli
+            (
+              (
+                dbo.NAMESERVER AS ns
+              INNER JOIN
+                dbo.TAXON_VERSION AS tvr
+              ON
+                ns.RECOMMENDED_TAXON_VERSION_KEY = tvr.TAXON_VERSION_KEY
+              )
+            INNER JOIN
+              dbo.TAXON AS tr
+            ON
+              tr.TAXON_KEY = tvr.TAXON_KEY
+            )
           ON
-            tv.TAXON_VERSION_KEY = tli.TAXON_VERSION_KEY
+            tv.TAXON_VERSION_KEY = ns.INPUT_TAXON_VERSION_KEY
           )
-        INNER JOIN
+        )
+      INNER JOIN
+        dbo.TAXON_LIST_ITEM AS tlir
+      ON
+        tlir.TAXON_LIST_ITEM_KEY = ns.RECOMMENDED_TAXON_LIST_ITEM_KEY
+      WHERE
+        t.LANGUAGE = '%s' AND
+        t.ITEM_NAME + ' ' + t.AUTHORITY IN (%s)",
+      language,
+      paste0("'", name, "'", collapse = ", ")
+    )
+  } else {
+    sql <- sprintf("
+      SELECT
+        t.ITEM_NAME AS InputName,
+        ns.RECOMMENDED_TAXON_VERSION_KEY as NBNKey,
+        tr.ITEM_NAME AS GenericName,
+        CASE
+          WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%%'
+          THEN 1 ELSE 0 END AS PreferenceInput,
+        CASE
+          WHEN tlir.TAXON_LIST_VERSION_KEY like 'INB%%'
+          THEN 1 ELSE 0 END AS PreferenceOutput,
+        tv.COMMENT AS Comment,
+        tv.Attribute
+      FROM
+        (
           (
             (
-              dbo.NAMESERVER AS ns
+              (
+                dbo.TAXON AS t
+              INNER JOIN
+                dbo.TAXON_VERSION AS tv
+              ON
+                t.TAXON_KEY = tv.TAXON_KEY
+              )
             INNER JOIN
-              dbo.TAXON_VERSION AS tvr
+              dbo.TAXON_LIST_ITEM AS tli
             ON
-              ns.RECOMMENDED_TAXON_VERSION_KEY = tvr.TAXON_VERSION_KEY
+              tv.TAXON_VERSION_KEY = tli.TAXON_VERSION_KEY
             )
           INNER JOIN
-            dbo.TAXON AS tr
+            (
+              (
+                dbo.NAMESERVER AS ns
+              INNER JOIN
+                dbo.TAXON_VERSION AS tvr
+              ON
+                ns.RECOMMENDED_TAXON_VERSION_KEY = tvr.TAXON_VERSION_KEY
+              )
+            INNER JOIN
+              dbo.TAXON AS tr
+            ON
+              tr.TAXON_KEY = tvr.TAXON_KEY
+            )
           ON
-            tr.TAXON_KEY = tvr.TAXON_KEY
+            tv.TAXON_VERSION_KEY = ns.INPUT_TAXON_VERSION_KEY
           )
-        ON
-          tv.TAXON_VERSION_KEY = ns.INPUT_TAXON_VERSION_KEY
         )
-      )
-    INNER JOIN
-      dbo.TAXON_LIST_ITEM AS tlir
-    ON
-      tlir.TAXON_LIST_ITEM_KEY = ns.RECOMMENDED_TAXON_LIST_ITEM_KEY
-    WHERE
-      t.LANGUAGE = '%s' AND
-      t.ITEM_NAME IN (%s)",
-    language,
-    paste0("'", name, "'", collapse = ", ")
-  ) %>%
+      INNER JOIN
+        dbo.TAXON_LIST_ITEM AS tlir
+      ON
+        tlir.TAXON_LIST_ITEM_KEY = ns.RECOMMENDED_TAXON_LIST_ITEM_KEY
+      WHERE
+        t.LANGUAGE = '%s' AND
+        t.ITEM_NAME IN (%s)",
+      language,
+      paste0("'", name, "'", collapse = ", ")
+    )
+  }
+  output <- sql %>%
     sqlQuery(
       channel = channel,
       stringsAsFactors = FALSE,
@@ -127,83 +187,4 @@ get_nbn_key <- function(name, language = "la", channel, authority = FALSE){
     warning("Duplicate matching keys")
   }
   return(output) #nocov end
-} else {
-    output <- sprintf("
-    SELECT
-      t.ITEM_NAME + ' ' + t.AUTHORITY AS InputName,
-      ns.RECOMMENDED_TAXON_VERSION_KEY as NBNKey,
-      tr.ITEM_NAME AS GenericName,
-      CASE
-        WHEN tli.TAXON_LIST_VERSION_KEY like 'INB%%'
-        THEN 1 ELSE 0 END AS PreferenceInput,
-      CASE
-        WHEN tlir.TAXON_LIST_VERSION_KEY like 'INB%%'
-        THEN 1 ELSE 0 END AS PreferenceOutput,
-      tv.COMMENT AS Comment,
-      tv.Attribute
-    FROM
-      (
-        (
-          (
-            (
-              dbo.TAXON AS t
-            INNER JOIN
-              dbo.TAXON_VERSION AS tv
-            ON
-              t.TAXON_KEY = tv.TAXON_KEY
-            )
-          INNER JOIN
-            dbo.TAXON_LIST_ITEM AS tli
-          ON
-            tv.TAXON_VERSION_KEY = tli.TAXON_VERSION_KEY
-          )
-        INNER JOIN
-          (
-            (
-              dbo.NAMESERVER AS ns
-            INNER JOIN
-              dbo.TAXON_VERSION AS tvr
-            ON
-              ns.RECOMMENDED_TAXON_VERSION_KEY = tvr.TAXON_VERSION_KEY
-            )
-          INNER JOIN
-            dbo.TAXON AS tr
-          ON
-            tr.TAXON_KEY = tvr.TAXON_KEY
-          )
-        ON
-          tv.TAXON_VERSION_KEY = ns.INPUT_TAXON_VERSION_KEY
-        )
-      )
-    INNER JOIN
-      dbo.TAXON_LIST_ITEM AS tlir
-    ON
-      tlir.TAXON_LIST_ITEM_KEY = ns.RECOMMENDED_TAXON_LIST_ITEM_KEY
-    WHERE
-      t.LANGUAGE = '%s' AND
-      t.ITEM_NAME + ' ' + t.AUTHORITY IN (%s)",
-    language,
-    paste0("'", name, "'", collapse = ", ")
-  ) %>%
-    sqlQuery(
-      channel = channel,
-      stringsAsFactors = FALSE,
-      as.is = TRUE
-    ) %>%
-    unique()
-
-  if (anyDuplicated(output$InputName) > 1) {
-    output <- output %>%
-      group_by_(~InputName) %>%
-      filter_(~PreferenceInput == max(PreferenceInput)) %>%
-      filter_(~PreferenceOutput == max(PreferenceOutput)) %>%
-      ungroup() %>%
-      select_(~-PreferenceInput, ~-PreferenceOutput) %>%
-      as.data.frame()
-  }
-  if (anyDuplicated(output$InputName) > 1) {
-    warning("Duplicate matching keys")
-  }
-  return(output) #nocov end
-}
 }
